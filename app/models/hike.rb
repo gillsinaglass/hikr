a97568806f314f03609f148f23269952a99fa544
gillsinaglass <rdgill95@gmail.com>
2019-02-11 12:31:20 -0500

    Hike Methods 2

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -18,10 +18,23 @@ class Hike < ApplicationRecord
 
   def length_of_hike
     if @end_time == "Pending"
-      return nil
+      @length_of_hike = nil
     else
       d = DateTime.parse((self.created_at - self.updated_at).to_s)
-      d.strftime('%M minutes')
+      @hike_minutes = d.strftime('%M')
+      @length_of_hike = @hike_minutes + " minutes"
     end
+  end
+
+  def get_trail_length
+    @trail_length = self.trail.distance
+  end
+
+  def avg_mile_pace
+    avg = @hike_minutes.to_f / @trail_length.to_f
+    sec = (avg.round(3) * 60).to_i
+    min, sec = sec.divmod(60)
+    hour, min = min.divmod(60)
+    return "%02d:%02d:%02d" % [hour, min, sec] + " per mile"
   end
 end
