80369c816be8f3eca088b140f8ecffeb63c56295
gillsinaglass <rdgill95@gmail.com>
2019-02-11 15:02:45 -0500

    Added User and review count

--- app/models/break.rb
+++ app/models/break.rb
@@ -2,6 +2,8 @@ class Break < ApplicationRecord
   belongs_to :user
   belongs_to :shelter
 
+  validates :review, presence: true, on: :update
+
   def start_time
     d = DateTime.parse(self.created_at.to_s)
     @start_time = d.strftime('%I:%M %p')
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,10 +2,22 @@ class Shelter < ApplicationRecord
   belongs_to :trail
   has_many :users, through: :breaks
 
-  def get_reviews
+  #returns breaks at a specific shelter
+  def breaks_at_shelter
     Break.all.select do |b|
       b.shelter_id == self.id
     end
   end
 
+  def review_blank
+    array = self.breaks_at_shelter.collect do |b|
+      if b.review == nil
+        array = nil
+      else
+        array = "#{b.user.username} - #{b.review}"
+      end
+    end
   end
+
+
+end
