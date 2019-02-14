fd7766dbee0ddcbfac74218f4c3fe4cbe82ff96c
gillsinaglass <rdgill95@gmail.com>
2019-02-14 12:58:02 -0500

    background

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "pages#home"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
@@ -6,8 +7,8 @@ Rails.application.routes.draw do
   resources :breaks, only: [:index, :show, :new, :create, :edit, :update]
   resources :trails, only: [:index, :show, :create, :new]
   resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :shelters, only: [:index, :show, :new, :create] 
-  
+  resources :shelters, only: [:index, :show, :new, :create]
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
